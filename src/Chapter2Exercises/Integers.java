f89df506ec4f62ef36f5e1aa21a38f13b6102357
Confidence Okere <okerenomso@gmail.com>
2021-05-22 16:04:49 +0100

    added another one called Integer

---
+++ src/Chapter2Exercises/Integers.java
@@ -0,0 +1,13 @@
+package Chapter2Exercises;
+
+public class Integers {
+    public static void main(String[] args) {
+
+        int largest = 9;
+        int smallest = 2;
+
+        System.out.println(" 2, 4, 6, 8, 9");
+        System.out.printf("%n%d is the Largest ", largest);
+        System.out.printf("%n%d is the Smallest ", smallest);
+    }
+}
