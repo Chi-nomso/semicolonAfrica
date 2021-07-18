1002629bf25d5052da1f6fe81bd9c25854ad3357
Confidence Okere <okerenomso@gmail.com>
2021-07-18 03:41:34 +0100

    still solving

--- src/Chapter4Exercises/Palindrome.java
+++ src/Chapter4Exercises/Palindrome.java
@@ -7,12 +7,25 @@ public class Palindrome {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a 5 digit number");
         int number = input.nextInt();
+        if(number / 10000 == 0){
+            System.out.println("This is not a 5digit number! Kindly enter a 5 digit number");
+            System.out.println("And");
+        }
 
-        int one = number%10000;
-        int b = one / 1000;
-        int two = b % 1000;
-        int c = two / 100;
-        int three = c % 100;
+        int one = number / 10000;
+        int a = number % 10000;
+        int two = a / 1000;
+        int b = a % 1000;
+        int three = b / 100;
+        int c = b % 100;
+        int four = c / 10;
+        int d = c % 10;
+
+        if(d == one && four == two){
+            System.out.println("This is a Palindrome");
+        }else{
+            System.out.print("This is not a Palindrome");
+        }
 
         }
 }
