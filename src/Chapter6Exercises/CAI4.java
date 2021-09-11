1c681c97505c6fc5ddb94d1fd04fb93d79d69b78
Confidence Okere <okerenomso@gmail.com>
2021-09-12 00:55:01 +0100

    Chapter6 point 39

--- src/Chapter6Exercises/CAI4.java
+++ src/Chapter6Exercises/CAI4.java
@@ -3,17 +3,17 @@ package Chapter6Exercises;
 import java.security.SecureRandom;
 import java.util.Scanner;
 
-public class CAI3 {
+public class CAI4 {
     /** ---> A Program that helps a child learn multiplication
-     * ---> Use SecureRandom object to generate 2 positive integers
+     * ---> Use SecureRandom object to generate 2 positive 2 digit integers
      * ---> Use a Method to Prompt the child with a question>> e.g How much is 2 times 4?
      * ---> check if the child's answer is correct:
      * ---> if the response is correct or wrong:
      use random number to choose between 1-4 to display:
-            i>>   Very good            ELSE DISPLAY:           i>> "No. Please try again"
-            ii>>  Excellent                                    ii>> Wrong. Try once more.
-            iii>> Nice work!                                   iii>> Don't give up!
-            iv>>  Keep up the good work!                       iv> No. Keep trying.
+     i>>   Very good            ELSE DISPLAY:           i>> "No. Please try again"
+     ii>>  Excellent                                    ii>> Wrong. Try once more.
+     iii>> Nice work!                                   iii>> Don't give up!
+     iv>>  Keep up the good work!                       iv> No. Keep trying.
      * ---> ask the child the same question till she gets it
      * ---> Keep and display the count of times of response(at least 10 times-- 100%)
      * ---> calculate and display the child's percentage, if below 75% print "Please ask your teacher for extra help." else display "Congratulations, you are ready to go to the next level!",
@@ -22,6 +22,34 @@ public class CAI3 {
     public static void quiz(int firstNum, int secondNum){
         System.out.println("How much is " + firstNum + " times " + secondNum);
     }
+
+
+    public static int arithmetic(int firstNum, int secondNum){
+        SecureRandom maths = new SecureRandom();
+        int question = 1 + maths.nextInt(5);
+        switch (question) {
+            case 1:
+                return firstNum * secondNum;
+            case 2:
+                return firstNum + secondNum;
+            case 3:
+                return firstNum - secondNum;
+            case 4:
+                return firstNum / secondNum;
+            case 5:
+                return firstNum % secondNum;
+            default:
+                switch (question) {
+                    case 1 -> System.out.println("How much is " + firstNum + " times " + secondNum);
+                    case 2 -> System.out.println("How much is " + firstNum + " plus " + secondNum);
+                    case 3 -> System.out.println("How much is " + firstNum + " minus " + secondNum);
+                    case 4 -> System.out.println("How much is " + firstNum + " divided by " + secondNum);
+                    case 5 -> System.out.println("How much is " + firstNum + " modulus " + secondNum);
+                }
+        }
+        return question;
+    }
+
     public static void goodComments(){
         SecureRandom numGen = new SecureRandom();
         int tip = 1 + numGen.nextInt(4);
@@ -43,6 +71,7 @@ public class CAI3 {
         }
     }
 
+
     public static void percentage(int times){
         int percent = times * 10;
         if(percent < 75){
@@ -55,9 +84,9 @@ public class CAI3 {
 
     public static void main(String[] args) {
         SecureRandom numGen = new SecureRandom();
-        int firstNum = numGen.nextInt(10);
-        int secondNum = numGen.nextInt(10);
-        int product = firstNum * secondNum;
+        int firstNum = 1 + numGen.nextInt(99);
+        int secondNum = 1 + numGen.nextInt(99);
+        arithmetic(firstNum,secondNum);
         Scanner input = new Scanner(System.in);
         int response = 0;
         int wrongCount= 0;
@@ -65,19 +94,18 @@ public class CAI3 {
         while (response != -1) {
             quiz(firstNum,secondNum);
             response = input.nextInt();
-            if (response != product) {
+            if (response != arithmetic(firstNum,secondNum)) {
                 badComments();
                 wrongCount++;
             } else {
                 goodComments();
                 rightCount++;
             }
-            firstNum = numGen.nextInt(10);
-            secondNum = numGen.nextInt(10);
-            product = firstNum * secondNum;
+             firstNum = 1 + numGen.nextInt(99);
+             secondNum = 1 + numGen.nextInt(99);
+             arithmetic(firstNum,secondNum);
         }
         System.out.println("You answered " + wrongCount + " wrongly, and answered "+ rightCount + " correctly");
         percentage(rightCount);
     }
 }
-
